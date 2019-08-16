b1fc6063a58f206de34077af073aab1ac5ed5721
Mendez324 <48069230+Mendez324@users.noreply.github.com>
2019-08-16 17:34:07 +0100

    first

---
+++ redtext/src/App.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+
+
+
+const Counter = ({count}) => {
+  return (
+    <div className="m-3">
+      <h1>{count}</h1>
+
+    </div>
+  );
+}
+
+const updateCounter = (dispatch, type) => {
+  return dispatch({type});
+  
+}
+
+
+
+class App extends Component {
+  
+  renderButtons = () => {
+    const { dispatch } = this.props;
+    return (
+      <div>
+        <button
+          className="mx-2 "
+          onClick={() => updateCounter(dispatch, 'INCREMENT')}>
+          +
+        </button>
+        <button
+          className="mx-2 "
+          onClick={() => updateCounter(dispatch, 'DECREMENT')}>
+          -
+        </button>
+      </div>
+    )
+  }
+
+  
+  
+  
+  render () {
+    const { count } = this.props;
+
+    return (
+      <div className="text-center">
+        <h1>The Counter App</h1>
+        <h2>Introduction to Redux</h2>
+          <Counter count={count}></Counter>
+        {this.renderButtons()
+        }
+
+
+
+      </div>
+    )
+
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    count: state.count,
+  }
+}
+
+export default connect(mapStateToProps)(App);
